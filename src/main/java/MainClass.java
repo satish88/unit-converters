d60c62fec28cb14650dff097fd68fd00e75cc40f
Satish Gupta <satish88@gmail.com>
2021-01-09 21:06:56 -0800

    committing gitignore file an MainClass

--- src/main/java/MainClass.java
+++ src/main/java/MainClass.java
@@ -1,2 +1,23 @@
 public class MainClass {
+    // members
+
+
+    // methods
+
+
+    // main method
+    public static void main(String[] args) {
+        System.out.println("What do you want to convert");
+        // 1. Weight
+        // 2. Distance
+        // 3. Temperature
+        // ...
+        System.out.println("Input and Output Units");
+        // 1. Celsius -> Fahrenheit
+        // 2. F -> Celsius
+        System.out.println("Enter input");
+        /// ---- some magic ---
+        System.out.println("Output: ");
+    }
+
 }
