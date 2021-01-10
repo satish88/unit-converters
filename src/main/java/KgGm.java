9f44070276c70ebacfc3d1f5087ee4c2371a3756
Satish Gupta <satish88@gmail.com>
2021-01-09 21:06:56 -0800

    UC-19 is able to change kg into gram

---
+++ src/main/java/KgGm.java
@@ -0,0 +1,14 @@
+public class KgGm {
+    public static void kilogram(int kg) {
+        int gram;
+        gram = 1000 * kg;
+        System.out.println(gram);
+    }
+
+    public static void main(String[] args) {
+        System.out.println();
+        kilogram(8);
+    }
+}
+
+
